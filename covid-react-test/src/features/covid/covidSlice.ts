bb108ebdd852889db5a6f52614b491792db000d8
がーみ <kagamiyuu22@gmail.com>
2021-07-07 05:36:55 +0900

    no message

--- covid-react-test/src/features/covid/covidSlice.ts
+++ covid-react-test/src/features/covid/covidSlice.ts
@@ -88,10 +88,8 @@ export const fetchAsyncGet = createAsyncThunk("covid/get", async () => {
 export const fetchAsyncGetDaily = createAsyncThunk(
   "covid/getDaily",
   async () => {
-    async () => {
       const { data } = await axios.get<APIDATADAILY>(`${apiUrl}/daily`);
       return data;
-    };
   }
 );
 
@@ -111,4 +109,31 @@ const covidSlice = createSlice({
   name: "covid",
   initialState: initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncGet.fulfilled, (state, action) => {
+      return {
+        ...state,
+        data:action.payload,
+      };
+    });
+    builder.addCase(fetchAsyncGetDaily.fulfilled, (state, action) => {
+      return {
+        ...state,
+        dailyData: action.payload,
+      };
+    });
+    builder.addCase(fetchAsyncGetCountry.fulfilled, (state, action) => {
+      return {
+        ...state,
+        data:action.payload.data,
+        country: action.payload.country,
+      };
+    });
+  },
 });
+
+export const selectData = (state: RootState) => state.covid.data;
+export const selectDailyData = (state: RootState) => state.covid.dailyData;
+export const selectCountry = (state: RootState) => state.covid.country;
+
+export default covidSlice.reducer;
